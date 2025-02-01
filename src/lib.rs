b0969c19e1aeedd75ed545deee149530dfa1f0d3
Brandon Ros <brandonros1@gmail.com>
2025-02-01 16:31:49 -0500

    split file

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,13 @@ mod span_guard;
 mod span_context;
 mod span_builder;
 mod utilities;
-mod metrics;
+mod gauge;
+mod counter;
 pub mod globals;
 pub mod logger;
 
 pub use tracer::OtlpTracer;
 pub use span_guard::SpanGuard;
 pub use structs::*;
-pub use metrics::{Counter, Gauge};
+pub use gauge::Gauge;
+pub use counter::Counter;
